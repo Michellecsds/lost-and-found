f9206a05a9b8d3db71ba666da00b7ccd102a2f6b
Hiu-Wai <chloeho248@gmail.com>
2025-01-25 18:17:22 +0000

    added history page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,14 +4,14 @@ import './Navbar.css';
 
 function Navbar() {
   return (
-    <div class = "container-fluid">
+    <div className="container-fluid">
     <nav className="navbar">
       <h1>Lost & Found</h1>
       <ul className="nav-links">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/Report">Report Lost Item</Link></li>
         <li><Link to="/Find">Found Items</Link></li>
-        <li><Link to="/my-items">My Items</Link></li>
+        <li><Link to="/History">History</Link></li>
       </ul>
     </nav>
     </div>
