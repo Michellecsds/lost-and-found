953db183e8dfd166831252f3137a6db3649b15b7
Hiu-Wai <chloeho248@gmail.com>
2025-01-25 19:07:19 +0000

    refined styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,30 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Navbar.css';
 
 function Navbar() {
   return (
     <div className="container-fluid">
     <nav className="navbar">
-      <h1>Lost & Found</h1>
+      <h1 class = "title">Lost & Found</h1>
       <ul className="nav-links">
-        <li><Link to="/">Home</Link></li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/" exact activeClassName="active">Home</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/Report" activeClassName="active">Report Lost Item</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/Find" activeClassName="active">Found Items</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/History" activeClassName="active">History</NavLink>
+            </li>
+          </ul>
+{/*         <li><Link to="/">Home</Link></li>
         <li><Link to="/Report">Report Lost Item</Link></li>
         <li><Link to="/Find">Found Items</Link></li>
-        <li><Link to="/History">History</Link></li>
-      </ul>
+        <li><Link to="/History">History</Link></li> */}
     </nav>
     </div>
   );
