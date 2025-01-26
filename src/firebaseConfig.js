01409d7334a90befb49fc4768ee3a7592daa5db0
Elinor Cheung <elinorcheung@hotmail.com>
2025-01-26 01:13:39 +0000

    fixed creds2

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,16 +1,9 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// Your Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAqqeVnSrttDtoJ0UKI-EMcGfzTAxu3JBY",
-  authDomain: "lost-and-found-dc831.firebaseapp.com",
-  projectId: "lost-and-found-dc831",
-  storageBucket: "lost-and-found-dc831.firebasestorage.app",
-  messagingSenderId: "330469284613",
-  appId: "1:330469284613:web:8c49e4f234aff6742d756c",
-  measurementId: "G-H5E519XD73",
-};
+require('dotenv').config();
+
+const apiKey = process.env.firebaseCreds;
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
